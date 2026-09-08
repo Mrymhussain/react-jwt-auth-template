8d4756468540f2000f2ec1812a1dfd8e11fde773
Maryam Hussain <202011514@stu.uob.edu.bh>
2026-09-08 16:36:32 +0300

    props

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -3,9 +3,7 @@ import { createContext, useState } from 'react';
 const UserContext = createContext();
 
 function UserProvider({ children }) {
-  // Create state just like you normally would in any other component
   const [user, setUser] = useState(null);
-
 
   const value = { user, setUser };
 
