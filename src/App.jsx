2a366f72c5922d3636cff8a1da6677de130c0740
Maryam Hussain <202011514@stu.uob.edu.bh>
2026-09-08 15:04:28 +0300

    signup form

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
+
+import { Routes, Route } from 'react-router';
 import NavBar from './components/NavBar/NavBar';
+import SignUpForm from './components/SignUpForm/SignUpForm';
 
 const App = () => {
   return (
     <>
       <NavBar />
-      <h1>Hello, friend!</h1>
+      <Routes>
+        <Route path='/sign-up' element={<SignUpForm />} />
+      </Routes>
     </>
   );
 };
+
 
 export default App;
