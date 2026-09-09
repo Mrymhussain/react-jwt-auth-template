6657477c405b0670fac25e5cab4511868cdfdc56
Maryam Hussain <202011514@stu.uob.edu.bh>
2026-09-09 09:26:19 +0300

    Nav Bar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,22 +1,33 @@
 import { useContext } from 'react';
 import { Link } from 'react-router';
 
-// Import the UserContext object
 import { UserContext } from '../../contexts/UserContext';
 
 const NavBar = () => {
+  const { user, setUser } = useContext(UserContext);
 
-  const { user } = useContext(UserContext);
+  const handleSignOut = () => {
+    localStorage.removeItem('token');
+    setUser(null);
+  };
 
   return (
     <nav>
       {user ? (
         <ul>
-          <li>Welcome, {user.username}</li>
+          <li>Hello, {user.username}</li>
+
+          <li>
+            <Link to="/" onClick={handleSignOut}>
+              Sign Out
+            </Link>
+          </li>
         </ul>
       ) : (
         <ul>
-          <li><Link to='/sign-up'>Sign Up</Link></li>
+          <li>
+            <Link to="/sign-up">Sign Up</Link>
+          </li>
         </ul>
       )}
     </nav>
